fbdb65e6ef7e7fc45a24d796e8d8d58bcd8ff922
Ty Cobb <ty.cobb.m@gmail.com>
2021-10-20 13:52:11 -0400

    add message / font

---
+++ Assets/Tarot/Message/Message.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+
+public class Message: MonoBehaviour {
+    // -- module --
+    /// get the module
+    public static Message Get {
+        get => FindObjectOfType<Message>();
+    }
+
+    // -- config --
+    [Header("config")]
+    [Tooltip("the root transform")]
+    [SerializeField] Transform m_Root;
+
+    [Tooltip("the message text label")]
+    [SerializeField] TMP_Text m_Text;
+
+    // -- tuning --
+    [Header("tuning")]
+    [Tooltip("the height at which the text begins to fade out")]
+    [SerializeField] float m_FallHeight = 7.5f;
+
+    [Tooltip("the height at which the text fades out completely")]
+    [SerializeField] float m_FadeEnd = 0.15f;
+
+    [Tooltip("the height at which the text begins to fade out")]
+    [SerializeField] float m_FadeStart = 1.15f;
+
+    // -- lifecycle --
+    void Start() {
+        StartFall();
+    }
+
+    void FixedUpdate() {
+        // fade out the text
+        m_Text.alpha = Mathf.InverseLerp(m_FadeEnd, m_FadeStart, m_Root.position.y);
+    }
+
+    // -- commands --
+    /// moves the message to the fall height
+    void StartFall() {
+        var p = m_Root.position;
+        p.y = m_FallHeight;
+        m_Root.position = p;
+    }
+}
